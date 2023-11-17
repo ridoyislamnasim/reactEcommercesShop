be0807ec552e62d41dee064ed4d08e7646f1b933
ridoyislamnasim <ridoyislamnasim@gmail.com>
2023-11-17 21:37:48 +0600

    img name

--- controller/admin/product/productsContrpller.js
+++ controller/admin/product/productsContrpller.js
@@ -9,9 +9,20 @@ const productschema = require("../../../models/product/productModel");
 const { createSlug, removeSlug } = require("../../common/function/common");
 const mongoose = require("mongoose");
 // remove image in file system
-const removeLocalImage = async (img) => {
-    console.log('removeLocalImage', img)
-    try { await fsPromises.unlink(`public${img}`) } catch (e) { console.log('not remove', e) }
+const removeLocalImage = async (imagePath) => {
+    // const imagePath = req.file.path;
+    console.log('remove =============', imagePath);
+    const removePath = `public/uploads/${imagePath}`
+    console.log('removePath', removePath);
+
+    try {
+        if (fs.existsSync(removePath)) {
+            fs.unlinkSync(removePath);
+        }
+        console.log("========= delteed")
+    } catch (error) {
+        console.log("=====not delteed")
+    }
 }
 // convert To Boolean
 const convertToBoolean = async (value) => {
@@ -32,29 +43,29 @@ const convertToBoolean = async (value) => {
 createProductController = async (req, res) => {
     console.log("req.body")
     console.log(req.body)
-    console.log(req.uploadedFiles)
-    console.log(req.uploadfields)
-    const fields = { ...req.uploadfields }
-    console.log(fields)
+    const fields = { ...req.body }
+    const imagePath = req.file.path
+    console.log(imagePath)
+    console.log("------------------------------------------------------", req.file)
     // const { category } = req.body;
     if (!fields.name || fields.name.trim() === '') {
-        await removeLocalImage(req.uploadedFiles[0])
+        await removeLocalImage(req.file.filename)
         res.json({ errorMsg: "name is required" });
     } else if (!fields.price || fields.price.trim() === '') {
-        await removeLocalImage(req.uploadedFiles[0])
+        await removeLocalImage(req.file.filename)
         res.json({ errorMsg: "price is required" });
     } else if (!fields.quantity || fields.quantity.trim() === '') {
-        await removeLocalImage(req.uploadedFiles[0])
+        await removeLocalImage(req.file.filename)
         res.json({ errorMsg: "quantity is required" });
     }
     // else if (!fields.shipping || fields.shipping.trim() === '') {
     //     res.json({ errorMsg: "shipping is required" });
     // } 
     else if (!fields.category || fields.category.trim() === '') {
-        await removeLocalImage(req.uploadedFiles[0])
+        await removeLocalImage(req.file.filename)
         res.json({ errorMsg: "category is required" });
     } else if (!fields.description || fields.description.trim() === '') {
-        await removeLocalImage(req.uploadedFiles[0])
+        await removeLocalImage(req.file.filename)
         res.json({ errorMsg: "description is required" });
     } else {
         try {
@@ -63,7 +74,7 @@ createProductController = async (req, res) => {
             const existsProduct = await productschema.findOne({ name: productName });
 
             if (existsProduct) {
-                await removeLocalImage(req.uploadedFiles[0])
+                await removeLocalImage(req.file.filename)
                 return res.json({ success: false, errorMsg: 'Product already exits' });
             }
             const slug = await createSlug(productName)
@@ -75,11 +86,9 @@ createProductController = async (req, res) => {
                     ...fields,
                     shipping: shippingBoolean,
                     slug: slug,
-                    image: `/${req.uploadedFiles[0]}`
+                    image: `uploads/${req.file.filename}`
                 });
                 const saveProduct = await createNewProduct.save()
-                // let savefile = `http://localhost:2000/uploads/${fileName}`
-
                 // Convert the Mongoose document to a plain JavaScript object
                 const plainProduct = saveProduct.toObject();
 
@@ -94,16 +103,16 @@ createProductController = async (req, res) => {
                     res.json({ success: true, message: 'Product Create successful', data: Extract });
                 }
             } catch (error) {
-                console.log("===========================================================================================", req.uploadedFiles[0])
-                await removeLocalImage(req.uploadedFiles[0])
+                console.log("===========================================================================================", req.file.path)
+                await removeLocalImage(req.file.filename)
                 console.error('Error:', error);
                 res.json({ success: false, errorMsg: 'Internal server error occurred save' });
 
             }
 
         } catch (error) {
-            console.log("===========================================================================================", req.uploadedFiles[0])
-            await removeLocalImage(req.uploadedFiles[0])
+            console.log("===========================================================================================", req.file.path)
+            await removeLocalImage(req.file.filename)
             console.error('Error:', error);
             res.json({ success: false, errorMsg: 'Internal server error occurred' });
         }
@@ -117,29 +126,28 @@ createProductController = async (req, res) => {
 // =========================== updateProductController===========================
 updateProductController = async (req, res) => {
     console.log("req.body")
-    // console.log(req.body)
+    console.log(req.body)
     // console.log(req.uploadedFiles)
-    console.log(req.uploadfields)
-    const fields = { ...req.uploadfields }
+    const fields = { ...req.body }
     // const { category } = req.body;
     if (!fields.name || fields.name.trim() === '') {
-        await removeLocalImage(req.uploadedFiles[0])
+        await removeLocalImage(req?.file?.path)
         res.json({ errorMsg: "name is required" });
     } else if (!fields.price || fields.price.trim() === '') {
-        await removeLocalImage(req.uploadedFiles[0])
+        await removeLocalImage(req?.file?.path)
         res.json({ errorMsg: "price is required" });
     } else if (!fields.quantity || fields.quantity.trim() === '') {
-        await removeLocalImage(req.uploadedFiles[0])
+        await removeLocalImage(req?.file?.path)
         res.json({ errorMsg: "quantity is required" });
     }
     // else if (!fields.shipping || fields.shipping.trim() === '') {
     //     res.json({ errorMsg: "shipping is required" });
     // } 
     else if (!fields.category || fields.category.trim() === '') {
-        await removeLocalImage(req.uploadedFiles[0])
+        await removeLocalImage(req?.file?.path)
         res.json({ errorMsg: "category is required" });
     } else if (!fields.description || fields.description.trim() === '') {
-        await removeLocalImage(req.uploadedFiles[0])
+        await removeLocalImage(req?.file?.path)
         res.json({ errorMsg: "description is required" });
     } else {
         try {
@@ -150,7 +158,7 @@ updateProductController = async (req, res) => {
             try {
                 await productschema.findById(productId);
             } catch (error) {
-                await removeLocalImage(req.uploadedFiles[0])
+                await removeLocalImage(req?.file?.path)
                 return res.json({ success: false, errorMsg: 'Product id  dose not exits' });
             }
             // Check  same product exists in the database
@@ -158,7 +166,7 @@ updateProductController = async (req, res) => {
                 const existsProduct = await productschema.find({ name: productName });
                 // length gater than 0 must this product exit
                 if (existsProduct.length > 1) {
-                    await removeLocalImage(req.uploadedFiles[0])
+                    await removeLocalImage(req.file.path)
                     return res.json({ success: false, errorMsg: 'Product already exits' });
                 } else if (existsProduct.length == 0) {
 
@@ -167,7 +175,7 @@ updateProductController = async (req, res) => {
                     //this id update this id product name same langth 1 
                     console.log("oneProduct", oneProduct.length)
                     if (oneProduct.length == 0) {
-                        await removeLocalImage(req.uploadedFiles[0])
+                        await removeLocalImage(req.file.path)
                         return res.json({ success: false, errorMsg: 'Product already exits same' });
                     }
                 }
@@ -179,16 +187,16 @@ updateProductController = async (req, res) => {
                 const slug = await createSlug(productName)
                 const shippingBoolean = await convertToBoolean(fields.shipping)
                 console.log('shippingBoolean', shippingBoolean);
-                console.log('fields?.image', fields?.image);
-                console.log('fields?.image', req?.uploadedFiles?.[0]);
+                // console.log('fields?.image', fields?.image);
+                // console.log('fields?.image', req?.uploadedFiles?.[0]);
                 let image
                 if (fields?.image) {
                     const parts = fields?.image.split('/');
                     const filename = parts[parts.length - 1];
-                    image = `/uploads/${filename}`
+                    image = `uploads/${filename}`
                     console.log('image', image);
                 } else {
-                    image = req.uploadedFiles[0]
+                    image = `uploads/${req.file.filename}`
                 }
                 const updateProduct = await productschema.findByIdAndUpdate(productId,
                     {
@@ -204,7 +212,6 @@ updateProductController = async (req, res) => {
                     await removeLocalImage(updateProduct.image)
                 }
 
-                // let savefile = `http://localhost:2000/uploads/${fileName}`
 
                 // Convert the Mongoose document to a plain JavaScript object
                 console.log('updateProduct', updateProduct);
@@ -215,7 +222,7 @@ updateProductController = async (req, res) => {
                     // Extract only the required fields
                     const Extract = {
                         ...plainProduct,
-                        image: nowUpdateProduct.image
+                        image: nowUpdateProduct.image,
                     }
                     console.log('Extract', Extract);
                     // update successful
@@ -223,16 +230,19 @@ updateProductController = async (req, res) => {
                 }
             } catch (error) {
                 console.error('Error:', error);
-                console.log("===========================================================================================", req.uploadedFiles[0])
-                await removeLocalImage(req.uploadedFiles[0])
+                console.log("===========================================================================================", req.file.path)
+                var parts = req.file.path.split('\\');
+                var last_part = parts[parts.length - 1];
+                console.log('last_part', last_part);
+                await removeLocalImage(last_part)
                 res.json({ success: false, errorMsg: 'Internal server error occurred save' });
 
             }
 
         } catch (error) {
-            console.log("===========================================================================================", req.uploadedFiles[0])
-            await removeLocalImage(req.uploadedFiles[0])
-            // try { await fsPromises.unlink(`public/${req.uploadedFiles[0]}`) } catch (e) { console.log('not remove', e) }
+            console.log("===========================================================================================", req.file.path)
+            await removeLocalImage(req?.file?.path)
+            // try { await fsPromises.unlink(`public/${req.file.path}`) } catch (e) { console.log('not remove', e) }
 
             console.error('Error:', error);
             res.json({ success: false, errorMsg: 'Internal server error occurred' });
